9bcec87c71820116ca5da623896b336c6933effc
Andres Patino <andrespa87@gmail.com>
2021-12-28 21:40:11 -0500

    added oninput attribute to trigger the change border function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const image = document.getElementById("imagen");
-const spacing = document.querySelector(".spacing");
+// const spacing = document.querySelector(".spacing");
 
-spacing.addEventListener("click", addBorder());
 
-function addBorder() {
-    image.style.borderWidth = spacing.value;
+function addBorder(borderValue) {
+    image.style.borderWidth = borderValue;
     image.style.borderColor = "red";
-    console.log("conectadoooooooooo")
+    console.log(borderValue);
 };
+
 
 
 // document.getElementById("imagen").style.border = '4em solid black';
