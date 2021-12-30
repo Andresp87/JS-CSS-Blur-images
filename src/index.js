35c7ad9bd6d295a07a057096aaf492b671ad6f16
Andres Patino <andrespa87@gmail.com>
2021-12-29 20:16:25 -0500

    working on JS - trying to modify blur effect

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-const image = document.getElementById("imagen");
+const image = document.getElementById("image-container");
 // const spacing = document.querySelector(".spacing");
 
 
 function addBorder(borderValue) {
-    image.style.borderWidth = borderValue;
+    image.style.borderWidth = `${borderValue}`;
     image.style.borderColor = "red";
+    image.style.filter = `blur(${blur.value})`
     console.log(borderValue);
 };
 
