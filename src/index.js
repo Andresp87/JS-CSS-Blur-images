7a71816b95b09f82e274288f99adc2d44b62861d
Andres Patino <andrespa87@gmail.com>
2021-12-30 23:04:55 -0500

    spacing and blur are working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-const image = document.getElementById("image-container");
+const image = document.querySelector("img");
 // const spacing = document.querySelector(".spacing");
 
 
 function addBorder(borderValue) {
-    image.style.borderWidth = `${borderValue}`;
-    image.style.borderColor = "red";
-    image.style.filter = `blur(${blur.value})`
+    image.style.border = `${borderValue}px solid red`;
     console.log(borderValue);
+    
 };
+
+function addBlur(blurValue) {
+    image.style.filter = `blur(${blurValue}px)`;
+
+}
 
 
 
